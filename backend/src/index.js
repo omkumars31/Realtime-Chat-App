8cba24cd9e36c7e25097d26e4ab7de0714e3e080
Om Kumar Singh <omkumar312003@gmail.com>
2025-06-23 16:13:42 +0530

    added pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,9 @@ import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
+import cors from "cors";
+
+
 
 dotenv.config()
 const app = express();
@@ -13,6 +16,11 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    
+}));
 
 
 app.use("/api/auth", authRoutes)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,57 @@
+import Navbar from "./components/Navbar"
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { axiosInstance } from "./lib/axios";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import {Loader} from "lucide-react";
+import "./index.css"
+import {Toaster} from "react-hot-toast";
+import ImageUploader from './components/ImageUploader';
 const App = () => {
-  return(
-    <div className="text-red-500">Hello World</div>
-  )
-}
+
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  useEffect(() => {
+  //console.log("calling checkAuth...");
+  checkAuth();
+}, [checkAuth]);
+//console.log("Rendering App =>", { authUser, isCheckingAuth });
+console.log({authUser});
+
+  if(isCheckingAuth && !authUser) 
+    return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  );
+
+return(
+    
+    <div>
+
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to= "/login"/> } />
+        <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage/>} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to= "/login"/> } />
+      </Routes>
+
+      <Toaster/>
+      <div>
+      <h1>Upload Demo</h1>
+      <ImageUploader />
+    </div>
+    </div>
+    
+  );
+};
+
 
 export default App;
